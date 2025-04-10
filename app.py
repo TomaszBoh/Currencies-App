9a060d5d29e033a18cfb2cddcb753644b6e5329a
TomaszBoh <boharewicz.tomasz@gmail.com>
2025-04-10 10:51:23 +0200

    Added landing page with table

--- app.py
+++ app.py
@@ -1,38 +1,40 @@
 from flask import Flask
 from flask import jsonify
 from flask import render_template
+from datetime import datetime, timedelta
+from utils.config import load_config
+from utils.db import close_db, load_data_from_db, open_db, currency_list
 
-#obiekt appki
+CONFIG_FILE = "config.yaml"
+
+config = load_config(CONFIG_FILE)
+
+#app object
 app = Flask("Walutomat")
 
-#dodajemy pierwsze routingi (adresy)
+#adding first routing
 @app.route("/")
+@app.route("/rate")
+@app.route("/rate/<currency>")
+@app.route("/rate/<currency>/<start_date>")
+@app.route("/rate/<currency>/<start_date>/<end_date>")
 
-def home_page():
-    return "Home Page"
 
-@app.route("/contact")
-def contact():
-    return "Contact Data"
+def rate(currency="eur", start_date=None, end_date=None):
+    today = datetime.now()
+    today_30_days_ago = today - timedelta(days=30)
 
-@app.route("/add/<int:a>/<int:b>")
-def add(a,b):
-    result = a +b 
-    response = {"a": a, "b": b, "sum":result}
-    return jsonify(response)
+    #if's to assign dates if not pass
+    if not start_date:
+        start_date = today_30_days_ago.strftime("%Y-%m-%d")
+    if not end_date:
+        end_date = today.strftime("%Y-%m-%d")
 
-@app.route("/template1")
-def template1():
-    return render_template("index.html")
-
-@app.route("/about")
-def about():
-    return render_template('about.html')
-
-@app.route("/price")
-def price():
-    return render_template('price.html')
-
+    db = open_db(config) #db connection
+    results = load_data_from_db(db, currency, start_date, end_date) #loading data
+    cur_list = currency_list(db)
+    close_db(db)
+    return render_template("page.html", data=results, currencies=cur_list)
 
 if __name__ == "__main__":
     app.run()
--- utils/db.py
+++ utils/db.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine, text
+from datetime import datetime, timedelta
 
 
 def make_connection_string(config):
@@ -37,3 +38,42 @@ def save_data_to_db(currency, db_conn):
         db_conn.commit()
     except Exception as e:
         print(f"Blad przy INSERT:\n{e}")
+
+
+def load_data_from_db(db_conn, currency, start_date, end_date):
+    params = {
+        "currency": currency.upper(),
+        "start_date": start_date,
+        "end_date": end_date,
+              }
+    
+    #SQL query
+    query = f"""SELECT date, exchange_rate
+                FROM rates
+                WHERE currency_code = '{currency.upper()}' AND date >= '{start_date}' AND date <= '{end_date}'
+                ORDER BY date ASC;"""
+    
+    try:
+        db_results = db_conn.execute(text(query), params)
+    except Exception as e:
+        print(f"Error durining downloading: \n {e}")
+        return []
+    
+    results = []
+    for r in db_results:
+        results.append({"date": r[0], "rate": r[1]})
+
+    return results
+    
+def currency_list(db_conn):
+    query_2 = f"""SELECT DISTINCT currency_code FROM rates"""
+
+    try:
+        db_results = db_conn.execute(text(query_2))
+    except Exception as e:
+        print(f"Error durining downloading: \n {e}")
+        return []
+    
+    cur_list = [r[0] for r in db_results]
+
+    return cur_list
