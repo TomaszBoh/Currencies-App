c63683e0c6ad6cc2fa03ea3ca58e45ef251c63a3
TomaszBoh <boharewicz.tomasz@gmail.com>
2025-04-11 09:52:38 +0200

    Finished template with charts and table

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def rate(currency="eur", start_date=None, end_date=None):
     return render_template("page.html", data=results, currencies=cur_list)
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000)
